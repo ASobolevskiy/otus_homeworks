31625d67c9e276051cc834993bff3b529e6c580b
asobolevskiy <asobolevskiy@evapps.ru>
2024-03-17 17:32:11 +0300

    Removed character fire responsibility from CharacterController, renamed in to CharacterDeathObserver

---
+++ Assets/Scripts/Character/CharacterDeathObserver.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace ShootEmUp
+{
+    public sealed class CharacterDeathObserver : MonoBehaviour
+    {
+        [SerializeField] 
+        private GameObject character; 
+        
+        [SerializeField] 
+        private GameManager gameManager;
+
+        private void OnEnable()
+        {
+            this.character.GetComponent<HitPointsComponent>().hpEmpty += this.OnCharacterDeath;
+        }
+
+        private void OnDisable()
+        {
+            this.character.GetComponent<HitPointsComponent>().hpEmpty -= this.OnCharacterDeath;
+        }
+
+        private void OnCharacterDeath(GameObject _) => this.gameManager.FinishGame();
+    }
+}
